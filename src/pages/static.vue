abaa48e35ab0a6f1dc1e94f2f10281f64079b61a
hemu <hemu@cmschina.com.cn>
2019-03-21 17:22:23 +0800

    static page

---
+++ src/pages/static.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+  page
+    a(:href='href' title='img'): img(:src='src' alt='img')
+</template>
+
+<script>
+export default {
+  name: 'pptr',
+  data() {
+    return {
+      href: `/${process.env.VUE_APP_NAME}`,
+      src: `/${
+        process.env.NODE_ENV === 'development' ? '' : process.env.VUE_APP_NAME
+      }${process.env.VUE_APP_ASSETS_DIR}/img/icons/apple-touch-icon.png`
+    }
+  }
+}
+</script>
